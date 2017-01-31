b193edff8d33e38c8a63c6aa42845c5f423794ae
Jonathan-Kirschbaum <jonathankirschbaum@gmail.com>
2017-01-30 19:36:51 -0500

    rainfall class
    for use with rainfallDemo

---
+++ rainfall.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class rainfall{//Start of class rainfall
+	public static void main(String[] args){//start of main method
+
+	double totalRain;
+	double avgRain;
+	int mostRain;
+	int leatRain;
+	final int num_Months = 12;
+
+	double[] yearlyRainfall = new double[num_Months];
+
+}//End of main method
+}//end of class rainfall
